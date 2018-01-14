ef2ab849963b98983280e4f1b911ed35f13fecb8
Thiago Bernardes <tmb0710@gmail.com>
2018-01-13 19:17:48 -0500

    Finishing interface

--- app/components/user-signup.js
+++ app/components/user-signup.js
@@ -2,8 +2,10 @@ import Component from '@ember/component';
 
 export default Component.extend({
 
+    isShowingSigninInstructions: true,
+
     actions: {
-        login() {
+        register() {
 
         }
     }
