b17d265743dd6638e7d666f2975b2bb09ae5c075
Thiago Bernardes <tmb0710@gmail.com>
2018-03-17 18:08:31 -0400

    Adding sales-transaction

--- app/components/sales-transactions.js
+++ app/components/sales-transactions.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    producers: ["9987366","9987367","9987356"],
+    productsIds: ["UC00098","UC00099"],
+    productsNames: ["Tomate Italiano", "Abaxaxi Mexicano", "Arroz Chines", "Madioca Brasileira"],
+    scales:["cx: 19 a 22kg", "cx grande: 30 a 50kg"],
+    selectedProducer: "",
+    selectedProductId: "",
+    selectedProductName: "",
+    selectScale: ""
 });
