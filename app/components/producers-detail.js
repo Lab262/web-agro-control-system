630fbed1241d9a64682574a2818b6902d3a8f28a
Thiago Bernardes <tmb0710@gmail.com>
2018-04-07 21:29:23 -0400

    Adding design of the producers list

--- app/components/producers-detail.js
+++ app/components/producers-detail.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+    producers: [
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" },
+        {identification: "UI00000000", name: "Nome", cpf:"000.000.000-00", cnpj: "000.000.000-00", lastTransaction: "00/00/00" }
+    ]
+
 });
