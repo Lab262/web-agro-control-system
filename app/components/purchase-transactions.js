974c54e69175fa584ad9f7f073f783ba7bb400f9
Thiago Bernardes <tmb0710@gmail.com>
2018-04-08 22:33:09 -0400

    Adding data formating

--- app/components/purchase-transactions.js
+++ app/components/purchase-transactions.js
@@ -8,5 +8,13 @@ export default Component.extend({
     selectedProducer: "",
     selectedProductId: "",
     selectedProductName: "",
-    selectScale: ""
+    selectScale: "",
+    unityPrice: "0",
+    productCost: "0",
+    historic: [
+        {cost: "R$ 1.800,00", productName: "Tomate Italiano", quantity: "40 cx (19 a 22 kg)", date: "00/00/00" },
+        {cost: "R$ 1.800,00", productName: "Tomate Italiano", quantity: "40 cx (19 a 22 kg)", date: "00/00/00" },
+        {cost: "R$ 1.800,00", productName: "Tomate Italiano", quantity: "40 cx (19 a 22 kg)", date: "00/00/00" },
+        {cost: "R$ 1.800,00", productName: "Tomate Italiano", quantity: "40 cx (19 a 22 kg)", date: "00/00/00" },
+    ],
 });
