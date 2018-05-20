d3f223b5bc93f25778beefb03de3fcc511118696
thiagomb10 <tbernardes@patterns.ca>
2018-05-20 14:37:29 -0400

    Adding supply graph in dashboard overview

--- app/components/dashboard-overview.js
+++ app/components/dashboard-overview.js
@@ -1,4 +1,20 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+    didInsertElement() {
+        this.setupOverallChart()
+    },
+
+    setupOverallChart() {
+        let chartData = {
+            labels: ["Limão Taití", "Tomate", "Uva", "Maxixe", "Tomate Italiano", "Melão", "Maça", "Batata Doce"],
+            lastMonthLabel: "Nov",
+            lastMonthData: [1, 2, 3, 4, 5, 6, 7, 8],
+            currentMonthLabel: "Dez",
+            currentMonthData: [4, 5, 7, 2, 1, 10, 12, 4]
+        }
+        this.set('overallChartData', chartData)
+    },
+
 });
