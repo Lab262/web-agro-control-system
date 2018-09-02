915406f8c5138c6f4ba5dbeff6ffee21c3cebe7f
luisresende <luisfrvilela@gmail.com>
2018-09-02 15:01:04 -0300

    Adding business rule on Report

--- app/routes/reports-container.js
+++ app/routes/reports-container.js
@@ -7,7 +7,33 @@ export default AuthenticatedRoute.extend({
     let store = this.store;
     return new Ember.RSVP.hash({
       currentUser: this.get('session.data.authenticated.currentUser'),
-      cooperative: store.findRecord('cooperative', this.get('session.data.authenticated.currentUser.data.cooperatives').map(item => item.cooperativeId)[0])
+      cooperative: store.findRecord('cooperative', this.get('session.data.authenticated.currentUser.data.cooperatives').map(item => item.cooperativeId)[0]),
+      getPurchaseTransaction: function(cooperativeId) {
+        return store.query('purchase-transaction', {
+          "where": {
+            "cooperative": {
+              "__type": "Pointer",
+              "className": "Cooperative",
+              "objectId": cooperativeId,
+            }
+          },
+          limit: 4,
+          include: 'product'
+        })
+      },
+      getSaleTransaction: function (cooperativeId) {
+        return store.query('sales-transaction', {
+          "where": {
+            "cooperative": {
+              "__type": "Pointer",
+              "className": "Cooperative",
+              "objectId": cooperativeId,
+            }
+          },
+          limit: 4,
+          include: 'product'
+        })
+      }
     });
   },
 
