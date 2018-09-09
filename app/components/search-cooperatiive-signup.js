d8312e5b600b758863b536a83ac2e5c8ca1abec6
luisresende <luisfrvilela@gmail.com>
2018-09-09 17:43:05 -0300

    Adding list of cooperatives on search cooperative signup

--- app/components/search-cooperatiive-signup.js
+++ app/components/search-cooperatiive-signup.js
@@ -2,9 +2,19 @@ import Component from '@ember/component';
 
 export default Component.extend({
     cooperatives: ["Cooperativa Cootaquara"],
-    actions: {
-        search() {
 
-        }
-    }
+    didInsertElement() {
+        this.loadData();
+    },
+
+    loadData(){
+        this.get('model').getCooperatives().then(cooperativesArray => {
+            let cooperativesApproved = cooperativesArray.content.filter(item => item.__data.isActive).map(item => item.getRecord());
+            this.set('cooperatives', cooperativesApproved);
+            console.log(cooperativesApproved);
+        }).catch(error => {
+            console.log(error);
+            this.loadData();
+        });
+    },
 });
