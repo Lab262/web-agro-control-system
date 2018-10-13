e4edd0be07b27fc7b3b74f26d5a0437d9192c982
thiagomb10 <tbernardes@patterns.ca>
2018-10-13 16:32:41 -0400

    Adding option to delete product

--- app/components/add-product-modal.js
+++ app/components/add-product-modal.js
@@ -12,6 +12,15 @@ export default Component.extend({
     actions: {
         closeModalAction() {
             this.onClose()
+        },
+        deleteItem(model) {
+            var confirm = window.confirm('Ao deletar o produto ' + model.get('name') + ', todas as transções associadas ao mesmo serão afetadas');
+            if (confirm) {
+                model.destroyRecord().then(deleted => {
+                    window.location.reload()
+                });
+            }
+
         }
     }
 });
