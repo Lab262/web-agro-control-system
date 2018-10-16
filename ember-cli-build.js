4f639f3c63d6f258b58a7763739e288fa219738b
thiagomb10 <tbernardes@patterns.ca>
2018-10-15 22:15:50 -0400

    ADding transaction date default

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,12 +4,13 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
-module.exports = function(defaults) {
-  
+module.exports = function (defaults) {
+
   let app = new EmberApp(defaults, {
     'ember-power-select': {
       theme: 'material'
     },
+
 
     sassOptions: {
       includePaths: ['bower_components/material-design-lite/src']
@@ -20,5 +21,6 @@ module.exports = function(defaults) {
     files: ['**/*.svg'],
     destDir: '/images'
   });
+
   return mergeTrees([app.toTree(), materialSVG]);
 };
